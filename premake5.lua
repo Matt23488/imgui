2bc55924b7d9f33e3bcfc60481d15f96dbc60ab5
Matthew Bryant <matt23488@gmail.com>
2019-07-15 10:10:58 -0400

    reverted my previous changes... Sometimes Windows is dumb.

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,8 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
+		cppdialect "C++17"
+		staticruntime "On"
 	
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -21,8 +23,13 @@ project "ImGui"
 	
 	filter "system:windows"
 		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "On"
 	
-	filter { "system:windows", "configurations:Release" }
-		buildoptions "/MT"
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		runtime "Debug"
+		symbols "on"
+		
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		runtime "Release"
+		optimize "on"
